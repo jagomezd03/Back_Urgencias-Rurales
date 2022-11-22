da0ffd02eb7f132cc915e274f415b7eaf6ce1030
jagomezd03 <88806021+jagomezd03@users.noreply.github.com>
2022-11-22 14:23:23 -0500

    Añadidas nuevas rutas

--- fire/routes/index.js
+++ fire/routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const pacientRouter = require('../controllers/pacientController')
-
+const pacientRouter = require('../controllers/pacientController');
+const doctorRouter = require('../controllers/doctorController');
+const companionRouter = require('../controllers/companionController');
+const diagnosticRouter = require('../controllers/diagnosticController');
+const consultationRouter = require('../controllers/consultationController');
 
 function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
     router.use('/pacients', pacientRouter);
-  }
-
+    router.use('/doctors', doctorRouter);
+    router.use('/companions', companionRouter);
+    router.use('/diagnostics', diagnosticRouter);
+    router.use('/consultations', consultationRouter)
+}
 
 module.exports = routerApi;
